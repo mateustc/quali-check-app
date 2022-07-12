2686b9aff16cb6c62c7f9829fd8dc92e82046bcc
Mateus <mateus@turyn.dev>
2022-07-12 11:02:58 -0300

    bundle

--- server.js
+++ server.js
@@ -4,12 +4,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static('www'));
 
-app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
-
 app.listen(PORT, function () {
   console.log(`Express server listening on port ${PORT}`);
 });
