9694c99cf98e993c41472d2f76eebe0c2a063f33
Mateus <mateus@turyn.dev>
2022-07-12 09:41:40 -0300

    prod

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './src/app/app.module';
+
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8081;
+
+enableProdMode();
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
 
 app.listen(PORT, () =>
   console.log(`Application is listening on port ${PORT}!`)
